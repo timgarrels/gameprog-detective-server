9ad3c224af6a380731527d784d37a6b800d390b6
Tim Garrels <tim.garrels@student.hpi.de>
2019-11-05 14:45:53 +0100

    Remove stupid flask attempt

--- User.py
+++
@@ -1,5 +0,0 @@
-from mongoengine import *
-
-class User(Document):
-    device_id = StringField(unique=True)
-    telegram_handle = StringField(unique=True)
--- setup.sh
+++
@@ -1,3 +0,0 @@
-# Create Mongodb detective-game
-# Create users table in detective-game
-# Create unqiue index deviceId and telegramId
