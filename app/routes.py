ed1554f1aa5cdb50e2ebd7eea44c5ea81ddbfb6b
Tim Garrels <tim.garrels@student.hpi.de>
2019-11-07 17:31:53 +0100

    Added data dump api route

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app
-from flask import Response, jsonify
+from flask import Response, jsonify, request
+import json
 from app.models import User
 from app import db
 
-
+# ---------- User Creation and Info ----------
 @app.route('/user/all')
 def all_users():
     # TODO: jsonify does not work on this object
@@ -41,4 +42,16 @@ def register_users_telegram_handle(user_id, telegram_handle):
         db.session.add(user)
         db.session.commit()
         return jsonify(True), 200
-    
+
+# ---------- User Data Dump ----------
+@app.route('/user/<user_id>/data', methods=['POST'])
+def recieve_user_data(user_id):
+    data = request.get_json()
+    if data.get('type') == 'contact':
+        return "CONTACT READING NOT IMPLEMENTED YET"
+    elif not data.get('type', None):
+        return "Please provide a tpye"
+    elif data.get('type') != 'contact':
+        return "I only take 'contact' as type"
+    return "Did not understand"
+
