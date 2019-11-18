1db65acedb342e6357b7268528d38c7cfc70fa0e
Tim Garrels <tim.garrels@student.hpi.de>
2019-11-18 13:16:16 +0100

    Updated API endpoints, creates user/all

--- app/routes.py
+++ app/routes.py
@@ -7,24 +7,24 @@ from app import db
 # ---------- User Creation and Info ----------
 @app.route('/user/all')
 def all_users():
-    # TODO: jsonify does not work on this object
-    return jsonify(User.query.all())
+    return jsonify({"userIds": [id[0] for id in User.query.with_entities(User.user_id).all()]})
 
 @app.route('/user/create')
 def create_user():
     user = User()
     db.session.add(user)
     db.session.commit()
-    return jsonify({"userID": user.user_id})
+    return jsonify({"userId": user.user_id})
 
 @app.route('/user/<user_id>')
 def get_user(user_id):
     # TODO: jsonify does not work on this object
     try:
         user = User.query.get(int(user_id))
+        return jsonify(user.to_dict()), 200
     except ValueError:
         # Invalid ID Type
-        return jsonify("Invalid userID"), 400
+        return jsonify("Invalid userId"), 400
 
 @app.route('/user/<user_id>/register/<telegram_handle>')
 def register_users_telegram_handle(user_id, telegram_handle):
@@ -32,7 +32,7 @@ def register_users_telegram_handle(user_id, telegram_handle):
         user = User.query.get(int(user_id))
     except ValueError:
         # Invalid ID Type
-        return jsonify("Invalid userID"), 400
+        return jsonify("Invalid userId"), 400
     if user.telegram_handle:
         # Telegram already registered for user
         return jsonify("Telegram already registerd for this user"), 400
@@ -41,17 +41,29 @@ def register_users_telegram_handle(user_id, telegram_handle):
         user.telegram_handle = telegram_handle
         db.session.add(user)
         db.session.commit()
-        return jsonify(True), 200
+        return jsonify("Successfull register"), 200
 
 # ---------- User Data Dump ----------
 @app.route('/user/<user_id>/data', methods=['POST'])
 def recieve_user_data(user_id):
-    data = request.get_json()
-    if data.get('type') == 'contact':
-        return "CONTACT READING NOT IMPLEMENTED YET"
-    elif not data.get('type', None):
-        return "Please provide a tpye"
-    elif data.get('type') != 'contact':
-        return "I only take 'contact' as type"
-    return "Did not understand"
 
+    data = request.get_json()
+
+    if not data:
+        return jsonify("Please provide data!"), 400
+
+    data_origin = data.get("origin", None)
+    data_type = data.get("type", None)
+
+    if not data_origin:
+        return jsonify("Please specify the data <origin>"), 400
+    if not data_type:
+        return jsonify("Please provide a <type>"), 400
+
+    if data_origin not in ["app", "bot"]:
+        return jsonify("I only take data <origin>ating from app or bot"), 400
+
+    if data_type not in ["contact"]:
+        return jsonify("I only handle contact data"), 400
+
+    return data["first_name"], 200
