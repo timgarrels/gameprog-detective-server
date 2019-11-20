2ea01b354268e64c94306f0907b5c98acd443e4c
tim.garrels <tim.garrels@gitlab.hpi.de>
2019-11-20 22:55:14 +0100

    Made bot API compliant. Handshake complete

--- bot_draft.py
+++ bot_draft.py
@@ -12,33 +12,31 @@ logging.basicConfig(level=logging.DEBUG,
 updater = Updater(token=Config.BOT_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
-def is_valid_auth(auth_key):
-    r = requests.get(Config.SERVER_URL + "/user/{}/register/{}".format(auth_key))
-    if auth_key in user_auth_association.values():
-        return True
-    return False
+def is_valid_start_token(start_token):
+    r = requests.get(Config.SERVER_URL + "/user/register?telegram_handle={handle}&telegram_start_token={token}".format(handle="Mock handle", token=start_token))
+    if r.status_code == 200:
+        return r, True
+    return r, False
 
 def start(update, context):
     try:
         auth_key = context.args[0]
-        if is_valid_auth(auth_key):
+        response, valid = is_valid_start_token(auth_key)
+        if valid:
             # Valid user auth key
-
-            for user, value in user_auth_association.items():
-                if value == auth_key:
-                    username = key
-                    break
-
-            context.bot.send_message(chat_id=update.effective_chat.id, text="Hello there, General {}".format(username))
+            context.bot.send_message(chat_id=update.effective_chat.id, text="Hello there, General Kenobi!")
+            context.bot.send_message(chat_id=update.effective_chat.id, text="You were registered!")
         else:
             # Invalid user auth key
             context.bot.send_message(chat_id=update.effective_chat.id, text="I don't know you!")
             context.bot.send_message(chat_id=update.effective_chat.id, text="I don't speak to strangers!")
+            context.bot.send_message(chat_id=update.effective_chat.id, text="<invalid token>")
 
     except IndexError:
         # No Auth key
         context.bot.send_message(chat_id=update.effective_chat.id, text="Who are you?")
         context.bot.send_message(chat_id=update.effective_chat.id, text="I don't speak to people who don't introduce themselves!")
+        context.bot.send_message(chat_id=update.effective_chat.id, text="<no token>")
 
 # Handl##ers
 start_handler = CommandHandler('start', start)
