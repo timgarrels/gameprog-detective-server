8a8b60a4e31f3320eca35f7eb412c1d9daf82e6b
Tim Garrels <tim.garrels@student.hpi.de>
2019-12-30 18:17:09 +0100

    Reordered app import setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,6 @@
 """Creates Server to provide Game API"""
+# TODO: The use of init code is discouraged
+# Also, app is a cyclic import app, app.routes, app
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
@@ -6,14 +8,14 @@ from flask_migrate import Migrate
 
 from config import Config
 
-# Setup API endpoint
-from app.routes.app_api import *
-from app.routes.bot_api import *
-from app.routes.debug_api import *
-
 # Create the flask app and the orm
 # TODO: Should 'app', 'db' and 'migrate' be uppercase? Do we need 'migrate'?
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+# Setup API endpoint
+from app.routes.app_api import *
+from app.routes.bot_api import *
+from app.routes.debug_api import *
