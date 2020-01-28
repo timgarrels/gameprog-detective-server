6fae315bbdd57b3d39c381b74b4db27325198280
Tim Garrels <tim.garrels@student.hpi.de>
2020-01-28 11:48:47 +0100

    Added task validator geo utlities

---
+++ app/story/utility.py
@@ -0,0 +1,35 @@
+import requests
+from xml.etree import ElementTree
+from geopy.distance import distance
+
+from app.models.game_models import User
+from app.models import utility
+
+
+def geo_at_street(lat, lon, street):
+    """Finds the closest address for given lat and lon and checks whether street is part of that adress
+    Note: The API is really picky, and the gras behind the HPI is not part of the HPI anymore."""
+    api_url = "https://nominatim.openstreetmap.org/reverse?lat={lat}&lon={lon}"
+    url = api_url.format(lat=lat, lon=lon)
+    resp = requests.get(url)
+    root = ElementTree.fromstring(resp.content)
+    for child in root:
+        if child.tag == "result":
+            if street in child.text:
+                return True
+    return False
+
+places = {"hpi": {
+    "coords": (52.394061, 13.131768),
+    "radius": 230,
+    },
+}
+def geo_close_to_place(lat, lon, place):
+    """Checks whether coords are in predefined radius to predefined place"""
+    place_dict = places.get(place, None)
+    if not place_dict:
+        raise ValueError("No such place: {}".format(place))
+    d = distance(place_dict["coords"], (lat, lon)).m
+    if d < place_dict["radius"]:
+        return True
+    return False
